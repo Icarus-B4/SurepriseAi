3896f38b8cb5ac9b9d7561d5c8b7b913975a702d
Icarus-B4 <icarus.mod56@gmail.com>
2026-06-16 22:06:32 +0200

    chore: bump version to v0.1.33

--- src/version.py
+++ src/version.py
@@ -3,6 +3,6 @@ version.py
 Zentrale Versions- und Release-Metadaten für SurepriseAi.
 """
 
-__version__ = "0.1.32"
+__version__ = "0.1.33"
 GITHUB_REPO = "Icarus-B4/SurepriseAi"
 APP_NAME = "SurepriseAi"
