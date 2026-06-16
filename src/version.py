9f058caeff1d8947ac846cfd6244f4c433ea02f7
Icarus-B4 <icarus.mod56@gmail.com>
2026-06-16 11:37:03 +0200

    chore: release v0.1.27

--- src/version.py
+++ src/version.py
@@ -3,6 +3,6 @@ version.py
 Zentrale Versions- und Release-Metadaten für SurepriseAi.
 """
 
-__version__ = "0.1.26"
+__version__ = "0.1.27"
 GITHUB_REPO = "Icarus-B4/SurepriseAi"
 APP_NAME = "SurepriseAi"
